45ae06bbff5bf69102ace9acc7a5bfba38cea3cf
Frederik Bolding <frederik.bolding@gmail.com>
2022-04-19 13:00:16 +0200

    Add basic signing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
+import { Wallet } from '@ethersproject/wallet'
+import { Web3Provider } from '@ethersproject/providers'
+import { getBIP44AddressKeyDeriver } from '@metamask/key-tree';
+
+const provider = new Web3Provider(wallet);
+
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
+  const coinTypeNode = await wallet.request({ method: 'snap_getBip44Entropy_966' });
+  const deriver = getBIP44AddressKeyDeriver(coinTypeNode);
+  const privKey = deriver(0).slice(0, 32);
+  const polygonWallet = new Wallet(privKey, provider);
   switch (requestObject.method) {
-    case 'hello':
-      return wallet.request({
+    case 'sign': {
+      const transaction = requestObject.params[0];
+      await wallet.request({
         method: 'snap_confirm',
         params: [
           {
-            prompt: `Hello, ${originString}!`,
+            prompt: `Do you want to sign this transaction?`,
             description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+              'foo bar',
+            textAreaContent: JSON.stringify(transaction),
           },
         ],
-      });
+      })
+      return polygonWallet.signTransaction(transaction);
+    }
     default:
       throw new Error('Method not found.');
   }
