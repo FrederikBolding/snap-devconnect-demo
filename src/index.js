1bfb32888f676b0c6db74cc758d143d54a8c33e6
Frederik Bolding <frederik.bolding@gmail.com>
2022-04-19 13:13:11 +0200

    Add transaction saving

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ import { getBIP44AddressKeyDeriver } from '@metamask/key-tree';
 
 const provider = new Web3Provider(wallet);
 
+async function getTransactions() {
+  const state = await wallet.request({ method: 'snap_manageState', params: ['get'] });
+  return state?.transactions ?? [];
+}
+
+async function saveTransaction(tx) {
+  const transactions = await getTransactions();
+  return wallet.request({ method: 'snap_manageState', params: ['update', { transactions: [...transactions, tx] }] })
+}
+
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
   const coinTypeNode = await wallet.request({ method: 'snap_getBip44Entropy_966' });
   const deriver = getBIP44AddressKeyDeriver(coinTypeNode);
   const privKey = deriver(0).slice(0, 32);
   const polygonWallet = new Wallet(privKey, provider);
   switch (requestObject.method) {
+    case 'getAddress': {
+      return polygonWallet.address;
+    }
+    case 'getTransactions': {
+      return getTransactions();
+    }
     case 'sign': {
       const transaction = requestObject.params[0];
       await wallet.request({
@@ -23,7 +39,9 @@ wallet.registerRpcMessageHandler(async (originString, requestObject) => {
           },
         ],
       })
-      return polygonWallet.signTransaction(transaction);
+      const signed = await polygonWallet.signTransaction(transaction);
+      await saveTransaction(signed);
+      return signed;
     }
     default:
       throw new Error('Method not found.');
